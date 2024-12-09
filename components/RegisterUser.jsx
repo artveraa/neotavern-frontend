0a02ae64a14f92e44a31db3b432a6b0db65b615e
Olivia <olivia.kzj@gmail.com>
2024-12-09 15:46:56 +0100

    add welcomeScreen, styleConstants

--- components/RegisterUser.jsx
+++ components/RegisterUser.jsx
@@ -3,7 +3,10 @@ import { View, Text } from "react-native";
 const RegisterUser = () => {
   return (
     <View>
-      <Text>Register user</Text>
+      <TextInput placeholder="Surnom" onChangeText={(value) => setNickname(value)} value={nickname} style={styles.input} /> 
+      <TextInput placeholder="Email" onChangeText={(value) => setMail(value)} value={mail} style={styles.input} /> 
+      <TextInput placeholder="Mot de passe" onChangeText={(value) => setPass(value)} value={pass} style={styles.input} /> 
+      <TextInput placeholder="Confirmation mot de passe" onChangeText={(value) => setConfirmationPass(value)} value={ConfirmationPass} style={styles.input} />
     </View>
   );
 };
--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -1,27 +1,107 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { SafeAreaView, View, Image, Text, StyleSheet, TouchableOpacity } from "react-native";
 import MapScreen from "./MapScreen";
 import RegisterScreen from "./RegisterScreen";
 import RegisterUser from "../components/RegisterUser";
+// import colors  from '../styleConstants/colors'
 
-const WelcomeScreen = ({ navigation }) => {
+const purple = '#EDA0FF'
+const green = '#E7FF98'
+
+const light = '#F5F5F5'
+const dark =  '#333333'
+const purpleBorder = '#E264FF'
+
+
+const WelcomeScreen = ({navigation}) => {
+
+  const handleSignup = () => {
+    console.log('clic')
+    return <RegisterUser/>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>WelcomeScreen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("TabNavigator")}>
-        <Text>Go to home</Text>
-      </TouchableOpacity>
-      <RegisterUser />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image source={require('../assets/icon.png')} style={styles.logo}/>
+
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.btnPrimary} onPress={() => navigation.navigate("TabNavigator")}>
+          <Image source={require('../assets/favicon.png')} style={styles.logoG}/>
+          <Text>Se connecter avec Google</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnPrimary} onPress={() => navigation.navigate("TabNavigator")}>
+          <Text>Se connecter avec Google</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.btnContainer}>
+        <Text>Pas encore inscrit ?</Text>
+        <TouchableOpacity style={styles.btnSecondary} onPress={() => handleSignup()}>
+          <Text>Créer un compte</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: light,
+    color: dark,
+    justifyContent: "space-between",
     alignItems: "center",
+
+    paddingTop:'25%',
+    paddingBottom:'25%',
+    paddingRight:28,
+    paddingLeft:28,
+
   },
+  logo:{
+    width:'40%',
+    height:'20%',
+  },
+  btnContainer:{
+    color: dark,
+
+    width:"100%",
+    justifyContent:'center',
+    alignItems:'center',
+    gap:24
+  },
+  logoG:{
+    width:'20%',
+    height:'100%',
+  },
+
+  btnPrimary:{
+    width:"100%",
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+
+    height:40,
+    padding: 4,
+
+    backgroundColor: light,
+    borderWidth: 1,
+    borderColor: purpleBorder,
+    borderRadius:15,
+  },
+  btnSecondary:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+
+    width:"100%",
+    height:40,
+    padding: 4,
+
+    backgroundColor: green,
+    borderRadius:15,
+  }
 });
 
 export default WelcomeScreen;
