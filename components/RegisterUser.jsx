fbc473910f809d7409227e6c1b34c916890359ae
Olivia <olivia.kzj@gmail.com>
2024-12-10 17:36:30 +0100

    add login et reducer user

--- components/RegisterUser.jsx
+++ components/RegisterUser.jsx
@@ -1,51 +1,121 @@
 import { useState } from "react";
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, } from "react-native";
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../reducers/user';
 
 import colors  from '../styleConstants/colors'
 
+
 const RegisterUser = ({navigation}) => {
+  //redux
+  const dispatch = useDispatch()
+  //input valeur
   const [nickname, setNickname] = useState('')
   const [mail, setMail] = useState('')
   const [pass, setPass] = useState('')
   const [confirmationPass, setConfirmationPass] = useState('')
+  //message erreur
+  const [emptyErr, setEmptyErr] = useState(false)
+  const [mailErr, setMailErr] = useState(false);
+  const [nameErr, setNameErr] = useState(false);
+  const mailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   const handleRegisterUser = () => {
-    // navigation.navigate('TabNavigator');
-    console.log('clicUser')
+    fetch('http://neotavern-backend.vercel.app/users/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ nickname: nickname, password: pass, email:mail}),
+    })
+    .then(response => response.json())
+    
+    .then(userData => {
+      if(userData.error === `Champs manquants ou vides`){
+        console.log('vide')
+        setEmptyErr({emptyErr:true})
+        return
+      }
+      if (userData.error === `Ce surnom est déjà existant`){
+        setNameErr(!nameErr)
+        console.log('ex')
+        return
+      }
+      if (!mailPattern.test(mail)){
+        setMailErr(mailErr)
+        console.log('mail')
+        return
+      }
+
+      dispatch(login({nickname: userData.nickname, token: userData.token}))
+      navigation.navigate("TabNavigator", { screen: 'MapScreen' })
+      });
   }
-
+  
   return (
-    <View>
-      <View>
-        <Text>Surnom</Text>
+    <View style={styles.container}>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Surnom
+        {nameErr && <Text style={styles.error}>  - nom déjà existant !</Text>}
+        </Text>
         <TextInput placeholder="Surnom" onChangeText={(value) => setNickname(value)} value={nickname} style={styles.input} /> 
-      </View>
 
-      <View>
-        <Text>E-mail de connexion</Text>
+
+        <Text style={styles.label}>E-mail de connexion
+        {mailErr && <Text style={styles.error}>  - mail erreur</Text>}
+        </Text>
         <TextInput placeholder="Email" onChangeText={(value) => setMail(value)} value={mail} style={styles.input} /> 
-      </View>
 
-      <View>
-        <Text>Mot de passe</Text>
+
+        <Text style={styles.label}>Mot de passe
+        </Text>
         <TextInput placeholder="Mot de passe" onChangeText={(value) => setPass(value)} value={pass} style={styles.input} /> 
-      </View>
 
-      <View>
-        <Text>Mot de passe</Text>
+
+        <Text style={styles.label}>Confirmer le mot de passe
+        </Text>
         <TextInput placeholder="Confirmation mot de passe" onChangeText={(value) => setConfirmationPass(value)} value={confirmationPass} style={styles.input} />
       </View>
+
+      {emptyErr && <Text style={[styles.label, styles.error]}> Des champs sont vides ! </Text>}
+
+      <TouchableOpacity style={styles.btnSecondary} onPress={() => handleRegisterUser()}>
+          <Text>Valider</Text>
+      </TouchableOpacity>
+        
     </View>
   );
 };
 
 const styles = StyleSheet.create({
 container: {
-  flex: 1,
   width:'100%',
-  backgroundColor:'red',
   justifyContent: "center",
   alignItems: "center",
+
+  marginTop:28,
+  marginBottom:28,
+},
+inputContainer:{
+  alignItems: "lef",
+  width:'85%',
+  margin:0,
+  padding:0
+},
+label:{
+  fontSize:12,
+},
+input:{
+  fontSize:14,
+
+  borderBottomColor:colors.dark,
+  borderBottomWidth: 1,
+  paddingTop: 4,
+  paddingBottom: 4,
+  marginBottom:24,
+},
+error:{
+  paddingBottom: 12,
+  color:colors.purpleBorder,
 },
 btnSecondary:{
   flexDirection:'row',
@@ -58,7 +128,8 @@ btnSecondary:{
 
   backgroundColor: colors.purple,
   borderRadius:15,
-}
+},
+
 })
 
 export default RegisterUser;
--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -1,76 +1,74 @@
 import React, { useState } from "react";
-import {
-  SafeAreaView,
-  View,
-  Image,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
-import MapScreen from "./MapScreen";
+import { useDispatch, useSelector } from 'react-redux';
+
+import { SafeAreaView, View, Image, Text, StyleSheet, TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 import RegisterUser from "../components/RegisterUser";
 import LoginUser from "../components/LoginUser";
 
-import colors from "../styleConstants/colors";
+import colors  from '../styleConstants/colors'
 
-import { ThemeContext } from "../context/ThemeContext";
-import { useContext } from "react";
+const WelcomeScreen = ({navigation}) => {
+	const user = useSelector((state) => state.user.value);
+  console.log(user)
 
-const WelcomeScreen = ({ navigation }) => {
-  const theme = useContext(ThemeContext);
+  //formulaire -> visible ou non 
+  const [resgisterForm, setResgisterForm]= useState(false)
+  const [loginForm, setLoginForm]= useState(false)
 
-  const [resgisterForm, setResgisterForm] = useState(false);
-  const [loginForm, setLoginForm] = useState(false);
-
-  const handleSignup = () => {
-    setResgisterForm(!resgisterForm);
-    if (resgisterForm) {
-      navigation.navigate("TabNavigator", { screen: "MapScreen" });
+  const handleSignupForm = () => {
+    setResgisterForm(!resgisterForm)    
+  }
+  const handleLoginForm = () => {
+    setLoginForm(!loginForm)
     }
-  };
-
-  const handleLogin = () => {
-    setLoginForm(!loginForm);
-    if (loginForm) {
-      navigation.navigate("TabNavigator", { screen: "MapScreen" });
-    }
-  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Image source={require("../assets/icon.png")} style={styles.logo} />
+      <Image source={require('../assets/icon.png')} style={styles.logo}/>
 
-      <View style={[styles.btnContainer, styles.btnContainerLogin]}>
-        <TouchableOpacity
-          style={styles.btnPrimary}
-          onPress={() => navigation.navigate("TabNavigator")}
-        >
-          <Image
-            source={require("../assets/favicon.png")}
-            style={styles.logoG}
-          />
-          <Text>Se connecter avec Google</Text>
+      {!resgisterForm &&
+            <View style={styles.btnContainer}>
+            <TouchableOpacity style={styles.btnPrimary} onPress={() => navigation.navigate("TabNavigator")}>
+              <Image source={require('../assets/favicon.png')} style={styles.logoG}/>
+              <Text style={[styles.l,styles.dark]}>Se connecter avec Google</Text>
         </TouchableOpacity>
 
-        {loginForm && <LoginUser navigation={navigation} />}
-        <TouchableOpacity
-          style={styles.btnPrimary}
-          onPress={() => handleLogin()}
-        >
-          <Text>Se connecter avec Google</Text>
+            {loginForm ?
+            <>
+            <LoginUser navigation={navigation}/>
+
+            <TouchableOpacity style={styles.back} onPress={() => handleLoginForm()}>
+              <Text style={[styles.s, styles.dark]}>
+              &lt; retour</Text>
+            </TouchableOpacity>
+            </>
+            :
+              <TouchableOpacity style={styles.btnPrimary}onPress={() => handleLoginForm()}>
+              <Text style={[styles.l, styles.dark]}>Se connecter avec son compte</Text>
         </TouchableOpacity>
+            }
       </View>
-
+      }
+      {!loginForm &&
       <View style={styles.btnContainer}>
-        <Text>Pas encore inscrit ?</Text>
-        {resgisterForm && <RegisterUser navigation={navigation} />}
-        <TouchableOpacity
-          style={styles.btnSecondary}
-          onPress={() => handleSignup()}
-        >
-          <Text>Créer un compte</Text>
+          <View style={styles.straight}></View>
+          <Text style={[styles.s, styles.center, styles.dark]}>Pas encore inscrit ?</Text>
+          {resgisterForm ?
+            <>
+            <RegisterUser navigation={navigation}/>
+            <TouchableOpacity style={styles.back} onPress={() => handleSignupForm()}>
+              <Text style={[styles.s, styles.dark]}>
+              &lt; retour</Text>
+            </TouchableOpacity>
+            </>
+            :
+            <TouchableOpacity style={styles.btnSecondary} onPress={() => handleSignupForm()}>
+            <Text style={[styles.l, styles.dark]}>Créer un compte</Text>
         </TouchableOpacity>
+          }
       </View>
+      }
     </SafeAreaView>
   );
 };
@@ -80,56 +78,80 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.light,
     color: colors.dark,
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingTop: "25%",
-    paddingBottom: "25%",
-    paddingRight: 28,
-    paddingLeft: 28,
-  },
-  logo: {
-    width: "40%",
-    height: "20%",
-  },
-  btnContainer: {
-    color: colors.dark,
-
-    width: "100%",
     justifyContent: "center",
+    gap:110,
     alignItems: "center",
-    gap: 24,
-  },
-  logoG: {
-    width: "20%",
-    height: "100%",
-  },
 
-  btnPrimary: {
-    width: "100%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop:'25%',
+    paddingBottom:'25%',
+    paddingRight:28,
+    paddingLeft:28,
+  },
+  dark:{
+    color:colors.dark
+  },
+  s:{
+    fontSize:12,
+  },
+  l:{
+    fontSize:16,
+  },
+  logo:{
+    width:'40%',
+    height:'20%',
+  },
+  btnContainer:{
+    alignItems:'center',
+    width:"100%",
+  },
+  logoG:{
+    width:'20%',
+    height:'100%',
+  },
+  straight:{
+    alignSelf:'center',
 
-    height: 40,
+    width:200,
+    height:1,
+
+    marginBottom:24,
+    backgroundColor:colors.purple,
+  },
+  btnPrimary:{
+    width:"100%",
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+
+    height:43,
     padding: 4,
+    marginTop: 12,
+    marginBottom:12,
 
+    fontSize:16,
     backgroundColor: colors.light,
     borderWidth: 1,
     borderColor: colors.purpleBorder,
-    borderRadius: 15,
+    borderRadius:15,
   },
-  btnSecondary: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+  btnSecondary:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
 
-    width: "100%",
-    height: 40,
+    width:"100%",
+    height:42,
     padding: 4,
+    marginTop: 12,
+    marginBottom:12,
 
+    fontSize:16,
     backgroundColor: colors.green,
-    borderRadius: 15,
+    borderRadius:15,
   },
+  back:{
+    width:'80%'
+  }
 });
 
 export default WelcomeScreen;
