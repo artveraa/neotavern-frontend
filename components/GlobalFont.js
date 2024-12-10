94164c89a63f2a7fbbf02a27debfec9bfa51bdde
Nicolas <blanc-nicolas@outlook.com>
2024-12-10 11:34:50 +0100

    GlobalFont typo dispo et début page event

--- components/GlobalFont.js
+++ components/GlobalFont.js
@@ -1,10 +1,41 @@
-'use strict';
-import { StyleSheet } from 'react-native';
+"use strict";
+import { StyleSheet } from "react-native";
+import {
+  useFonts,
+  Lexend_900Black,
+  Lexend_800ExtraBold,
+  Lexend_700Bold,
+  Lexend_600SemiBold,
+  Lexend_500Medium,
+  Lexend_400Regular,
+  Lexend_300Light,
+  Lexend_200ExtraLight,
+  Lexend_100Thin,
+} from "@expo-google-fonts/lexend";
 
-module.exports = StyleSheet.create({
-    alwaysred: {
-        backgroundColor: 'red',
-        height: 100,
-        width: 100,
-    },
-});
+const GlobalFont = ({children}) => {
+  let [fontsLoaded] = useFonts({
+    Lexend_900Black,
+    Lexend_800ExtraBold,
+    Lexend_700Bold,
+    Lexend_600SemiBold,
+    Lexend_500Medium,
+    Lexend_400Regular,
+    Lexend_300Light,
+    Lexend_200ExtraLight,
+    Lexend_100Thin
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View >
+      {children} 
+    </View>
+  );
+};
+
+
+export default GlobalFont;
--- screens/AddEventScreen.jsx
+++ screens/AddEventScreen.jsx
@@ -1,10 +1,48 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from 'react';
+import { View, Text, StyleSheet,Button, TextInput} from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
 const AddEventScreen = () => {
+  const [eventName, setEventName] = useState("");
+  const [eventType, setEventType] = useState('Musique');
+  const [eventText, setEventText] = useState("");
+  const [eventDate, setEventDate] = useState("");
+  const [eventHour, setEventHour] = useState("");
+  const [eventPrice, setEventPrice] = useState("");
+  const [eventDrinks, setEventDrinks] = useState("");
+  const [eventFood, setEventFood] = useState("");
+  const [eventAge, setEventAge] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>AddEventScreen</Text>
+      <View>
+      <Text style={styles.label}>Nom de l'événement:</Text>
+      <TextInput
+        onChangeText={(value) => setEventName(value)}
+        value={eventName}
+      />
+      </View>
+      <View>
+        <Text style={styles.label}>Type d'événement:</Text>
+        <Picker
+          selectedValue={eventType}
+          style={styles.picker}
+          onValueChange={(value) => setEventType(value)}
+        >
+          <Picker.Item label="Musique" value='musique' />
+          <Picker.Item label="Foot" value='foot' />
+          <Picker.Item label="Jeux" value='jeux' />
+          <Picker.Item label="Nouvel an" value='nouvelan' />
+        </Picker>
+      </View>
+      <View>
+      <Text style={styles.label}>Description:</Text>
+      <TextInput
+        onChangeText={(value) => setEventText(value)}
+        value={eventText}
+      />
+      </View>
     </View>
   );
 };
