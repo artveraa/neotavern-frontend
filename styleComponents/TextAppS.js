4575f417ac022e772c2533e66dd3ef13792029a8
Olivia <olivia.kzj@gmail.com>
2024-12-11 10:37:00 +0100

    add styleText

---
+++ styleComponents/TextAppS.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text } from "react-native";
+
+const TextAppS= ({children}) => {
+    return (
+        <Text style={{color:'#333333', fontFamily:'Lexend_300Light', size:'12'}}>{children}</Text>
+    );
+  };
+export default TextAppS
