13f8dc7fd358b6ac429a1e2ed6607681ee981230
Nicolas <blanc-nicolas@outlook.com>
2024-12-16 09:03:40 +0100

    TDD Nico searchPlace.test.js

--- searchPlace.test.js
+++ searchPlace.test.js
@@ -4,14 +4,15 @@ const placesList = [
   { name: "bar sans alcool" },
 ];
     
+// cette fonction a pour but de trier les places en fonction d'une recherche par caracteres
   const filteredPlaces = (places, search) => {
     return places.filter((place) =>
        place.name.toLowerCase().includes(search.toLowerCase())
     );
   }
     
+// ce test vérifie que le nom tapé correspond bien à la liste d'établissements (que ce soit en majuscule ou en minuscule)
 it("la fonction filterPlaces devrait chercher correctement le lieu selon la recherche tapée", () => {
-
   const result = filteredPlaces(placesList, "Restaurant");
   expect(result).toEqual([{ name: "restaurant Français" }]);
 });
