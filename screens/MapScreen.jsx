18f961a9aedb4d2d800960b1a61a2badd397a9d0
arthurvera <arthurvera96@gmail.com>
2024-12-17 16:08:01 +0100

    delete log

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -68,7 +68,6 @@ const MapScreen = ({ navigation }) => {
   const fetchEvents = async () => {
     try {
       const events = await getAllEvents();
-      console.log('EVENTSSS:', events)
       setAllEvents(events);
     } catch (error) {
       console.error(error);
