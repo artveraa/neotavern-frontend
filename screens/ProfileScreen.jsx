9226242047fbc2f588ffb0920e8711516021eb37
Nicolas <blanc-nicolas@outlook.com>
2024-12-19 11:16:06 +0100

    cleanCode ProfileScreen Done

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,39 +1,36 @@
-import { useRef, useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useSelector } from "react-redux";
+import { useFocusEffect } from "@react-navigation/native";
 import {
   SafeAreaView,
   View,
   Text,
   StyleSheet,
   ScrollView,
-  Image,
   TouchableOpacity,
   ActivityIndicator,
 } from "react-native";
-import { useFocusEffect } from "@react-navigation/native";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
-import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
 
-import TextApp from "../styleComponents/TextApp";
-import TextAppBold from "../styleComponents/TextAppBold";
-import TextAppTitle from "../styleComponents/TextAppTitle";
 import { getAllEvents } from "../fetchers/events";
-import CardEvent from "../components/CardEvent";
-import colors from "../styleConstants/colors";
 import CardEventProfil from "../components/CardEventProfil";
 
+import TextApp from "../styleComponents/TextApp";
+import TextAppTitle from "../styleComponents/TextAppTitle";
+import colors from "../styleConstants/colors";
+
 const ProfileScreen = ({ navigation }) => {
+  // userEvents contient les événéments créés seulement par l'utilisateur
   const [userEvents, setUserEvents] = useState([]);
   // isLoading pour charger les événements créés par l'utilisateur
   const [isLoading, setIsLoading] = useState(true);
 
+  // import des données utilisateurs depuis le reducer
   const user = useSelector((state) => state.user.value);
   const nickname = user.user.nickname;
   const email = user.user.email;
   const token = user.user.token;
-  const id = user.user.id;
 
-  // Simuler un chargement avec un délai de 2 secondes
+  // Simuler un chargement avec un délai de 1,5 secondes
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
@@ -41,29 +38,32 @@ const ProfileScreen = ({ navigation }) => {
     return () => clearTimeout(timer); // Nettoyer le timer
   }, []);
 
+  // importe tous les événements de la base de donnée et tri ceux qui sont créés par l'utilisateur (avec son token)
+    // ajout d'un try catch pour la gestion des erreurs
   const fetchEvents = async () => {
     try {
       const events = await getAllEvents();
-
       if (user) {
-        // Vérifie si `user` n'est pas nul
+        // Vérifie si 'user' n'est pas nul
         const myEvents = events.filter(
           (event) => event.user && event.user.token === token
         );
-        setUserEvents(myEvents);
+        setUserEvents(myEvents); // ajoute les événéments créés par l'utilisateur dans l'état userEvents
       }
     } catch (error) {
       console.error(error);
     }
   };
 
+  // Actualisation du fetch quand l'utilisateur change de screen
+    // useCallBack memorise le fetch pour éviter qu'il soit re-render à chaque rendu
   useFocusEffect(
     useCallback(() => {
       fetchEvents();
     }, [])
   );
-  // console.log("recup des events:", userEvents);
 
+  // Supprime un événement créé par l'utilisateur (identifié par son token) grâce au fetch de la route Delete (events) en back
   const handleDeleteEvent = (eventId) => {
     fetch(`https://neotavern-backend.vercel.app/events/deleteEvent/${token}`, {
       method: "DELETE",
@@ -84,14 +84,14 @@ const ProfileScreen = ({ navigation }) => {
       });
   };
 
+  // Supprime le compte de l'utilisateur (identifié par son token) grâce au fetch de la route Delete (users) en back
   const handleDeleteUser = (token) => {
     fetch(`https://neotavern-backend.vercel.app/users/deleteUser/${token}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-      }),
+      body: JSON.stringify({}),
     })
       .then((response) => response.json()) // Conversion de la réponse en JSON
       .then((data) => {
@@ -111,56 +111,50 @@ const ProfileScreen = ({ navigation }) => {
         <View style={styles.userInfo}>
           <TextApp>Surnom: {nickname}</TextApp>
           <TextApp>Email: {email}</TextApp>
-          {/* <TouchableOpacity style={styles.buttonReset}>
-            <TextApp>Réinitialiser mon mot de passe</TextApp>
-          </TouchableOpacity> */}
         </View>
       </View>
-      {/* <View style={styles.blockBadges}>
-        <TextAppTitle>Mes badges</TextAppTitle>
-        <View>
-          <Image source="../assets/badge" style={styles.image} />
-        </View>
-      </View> */}
       <View style={styles.events}>
-        <TextAppTitle>Mes événements créées</TextAppTitle>
-          {isLoading ? (
-            <View style={styles.loading}>
-              <ActivityIndicator
-                size="large"
-                color={colors.dark}
-              />
-            </View>
-          ) : (
-        <ScrollView style={styles.scrollWrapper}>
-          <View style={styles.eventContent}>
-          <TextApp>
-              Gérez vos évènements créés ici !
-            </TextApp>
+        <TextAppTitle>Mes événements créés</TextAppTitle>
 
-            {userEvents &&
-            userEvents
-              .sort((a, b) => new Date(a.date) - new Date(b.date))
-              .map((event) => (
-                <View key={event._id} style={styles.card}>
-                  <CardEventProfil
-                    key={event._id}
-                    event={event}
-                    navigation={navigation}
-                    handleDeleteEvent={handleDeleteEvent}
-                  />
-                </View>
-              ))}
+        {/* Ajout d'un loader de 1,5 sec pour montrer que les événéments que l'on a créé se charges */}
+
+        {isLoading ? (
+          <View style={styles.loading}>
+            <ActivityIndicator size="large" color={colors.dark} />
           </View>
-        </ScrollView>
-          )}
-        <TouchableOpacity style={styles.buttonDelete} onPress={() => handleDeleteUser(token)}>
+        ) : (
+          <ScrollView style={styles.scrollWrapper}>
+            <View style={styles.eventContent}>
+              <TextApp>Gérez vos évènements créés ici !</TextApp>
+
+              {/* Ajout (map) et Tri (sort) des événéments créés par l'utilisateur par ordre de création (nouveaux en premier) */}
+
+              {userEvents &&
+                userEvents
+                  .sort((a, b) => new Date(a.date) - new Date(b.date))
+                  .map((event) => (
+                    <View key={event._id} style={styles.card}>
+                      <CardEventProfil
+                        key={event._id}
+                        event={event}
+                        navigation={navigation}
+                        handleDeleteEvent={handleDeleteEvent}
+                      />
+                    </View>
+                  ))}
+
+            </View>
+          </ScrollView>
+        )}
+        <TouchableOpacity
+          style={styles.buttonDelete}
+          onPress={() => handleDeleteUser(token)}
+        >
           <TextApp>Supprimer mon compte</TextApp>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
-  
 };
 
 const styles = StyleSheet.create({
@@ -189,8 +183,8 @@ const styles = StyleSheet.create({
   },
   blockInfo: {
     justifyContent: "flex-start",
-    alignItems: 'center',
-    width: '100%',
+    alignItems: "center",
+    width: "100%",
     marginTop: 28,
     padding: 28,
     borderWidth: 0.3,
@@ -199,9 +193,6 @@ const styles = StyleSheet.create({
   userInfo: {
     paddingTop: 12,
   },
-  // blockBadges: {
-  //   paddingTop: 30,
-  // },
   scrollWrapper: {
     minWidth: "100%",
     marginVertical: 28,
@@ -213,7 +204,6 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
   },
   events: {
-    // flex:1,
     paddingTop: 32,
     justifyContent: "center",
     alignItems: "center",
@@ -232,7 +222,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.red,
   },
   buttonReset: {
-    flex:1,
+    flex: 1,
     marginTop: 10,
     marginLeft: 20,
     padding: 12,
@@ -242,12 +232,12 @@ const styles = StyleSheet.create({
   eventContent: {
     borderRadius: 15,
     width: "100%",
-    
-    alignItems:'center',
-    borderWidth:0.3,
-    borderColor:colors.dark,
 
-    padding:12,
+    alignItems: "center",
+    borderWidth: 0.3,
+    borderColor: colors.dark,
+
+    padding: 12,
   },
 });
 
